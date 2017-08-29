2fb3eb6109462baec9395970d201d8d9ef897b16
egil <egil@localhost>
2017-08-29 14:17:13 -0500

    Correct code calling simple 2

--- Model/Payment/Simple.php
+++ Model/Payment/Simple.php
@@ -6,5 +6,10 @@ namespace CDS\TDCPayment\Model\Payment;
 
 class Simple extends \Magento\Payment\Model\Method\AbstractMethod
 {
+    /**
+     * Payment code
+     *
+     * @var string
+     */
     protected $_code = 'simple';
 }
