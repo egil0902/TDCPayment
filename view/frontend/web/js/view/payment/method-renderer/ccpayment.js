737d1089f030ec5c28609e3a0cf7228141b3d073
egil <egil@localhost>
2017-08-29 21:31:59 -0500

    Add credit card form in front end

--- view/frontend/web/js/view/payment/method-renderer/ccpayment.js
+++ view/frontend/web/js/view/payment/method-renderer/ccpayment.js
@@ -1,6 +1,12 @@
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+        'Magento_Checkout/js/view/payment/default',
+        'Magento_Payment/js/view/payment/cc-form',
+        'jquery',
+        'Magento_Checkout/js/action/place-order',
+        'Magento_Checkout/js/model/full-screen-loader',
+        'Magento_Checkout/js/model/payment/additional-validators',
+        'Magento_Payment/js/model/credit-card-validation/validator'
     ],
     function (Component) {
         'use strict';
@@ -8,6 +14,18 @@ define(
         return Component.extend({
             defaults: {
                 template: 'CDS_CCPayment/payment/ccpayment'
+            },
+             getCode: function() {
+                return 'stripe';
+            },
+
+            isActive: function() {
+                return true;
+            },
+
+            validate: function() {
+                var $form = $('#' + this.getCode() + '-form');
+                return $form.validation() && $form.validation('isValid');
             }
         });
     }
